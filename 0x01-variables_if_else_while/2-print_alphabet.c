b283468c0b265959a49fce0e73068d2873527a62
chukwumaGrace <chukwumagraceug@gmail.com>
2023-03-18 16:04:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cl;
+
+	for (cl = 'a'; cl <= 'z'; cl++)
+	{
+		putchar(cl);
+	}
+	putchar('\n');
+	return (0);
+}
