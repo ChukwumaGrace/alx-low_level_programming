f6f9def7e644942bb4a852f973ea025aeb19be24
chukwumaGrace <chukwumagraceug@gmail.com>
2023-03-27 11:51:27 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Entry point
+ * function that returns the length of a string
+ * @s: string pointer
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
