0c60b39355fa0c588742a2b0bb41a44b7373078d
chukwumaGrace <chukwumagraceug@gmail.com>
2023-04-05 17:47:03 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,5 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 
-#endif
+#endif /* MAIN_H */
 
